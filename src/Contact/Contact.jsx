a622089880d8a2be17afb88258f56f39672ffbce
Shubhamkumarkaushik1799 <shubhamkaushik1799@gmail.com>
2023-10-19 18:56:08 +0530

    added module css files

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,9 @@
-import "./Contact.css";
+import styles from "./Contact.module.css";
 import React from "react";
 
 function template() {
   return (
-    <div className="contact">
+    <div className={`contact ${styles.clr}`}>
       <h1>Contact</h1>
     </div>
   );
